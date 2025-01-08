9cc34d48bdf02be1eeb2a0afc43ce6eb62c9bc3d
QUANTUM DEV <107327640+Vuleegg@users.noreply.github.com>
2025-01-08 19:35:33 +0100

    Update cl_main.lua

--- qt-alert/cl_main.lua
+++ qt-alert/cl_main.lua
@@ -1,5 +1,15 @@
 ---@diagnostic disable: missing-parameter
 
+--- Sends an announcement to the NUI interface.
+---@param data table A table containing the announcement data.
+---@field position string The position of the announcement on the screen (e.g., "top-center").
+---@field progressColor string The color of the progress bar (e.g., "white").
+---@field iconColor string The color of the icon (e.g., "white").
+---@field backgroundColor string The background color of the announcement box (e.g., "rgba(11, 12, 13, 0.8)").
+---@field title string The title of the announcement (e.g., "Quantum Roleplay").
+---@field text string The text content of the announcement (e.g., "This is a test message").
+
+
 SendAnnouncment = function(data) 
     if not data then return end 
 
